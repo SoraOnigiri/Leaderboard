1f269dfe921987d3aa40729d35cf1b93208084af
aaron <94424462+SoraOnigiri@users.noreply.github.com>
2022-01-22 16:09:54 -0600

    Update calculations

--- scripts/leaderboard.py
+++ scripts/leaderboard.py
@@ -291,7 +291,7 @@ class User(commands.Cog):
         user = userCol.find_one({"userid": userid})
         balance = user["balance"]
         debt = user["debt"]
-        percentage = ((balance - debt) / 100000) * 100
+        percentage = (balance - debt) / 100000
 
         return round(percentage, 2)
 
@@ -1040,7 +1040,7 @@ class User(commands.Cog):
             user = i["name"]
             balance = round(i["balance"], 2)
             debt = round(i["debt"], 2)
-            total = round(((balance - debt) / 100000) * 100, 2)
+            total = round(((balance - debt) / 100000), 2)
 
             trade = await self.get_trade_number(i["userid"], str(ctx.message.guild.id))
 
